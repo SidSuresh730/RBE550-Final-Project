6be7694a6936880b8f4648c82e3017360449401c
Sid Suresh <ssuresh@wpi.edu>
2023-02-20 21:36:04 -0500

    Implemented basic collision-detection

--- rrt_robot.py
+++ rrt_robot.py
@@ -11,7 +11,7 @@ import maze_generation
 # Input: Nodes
 # Output: float (Distance)
 def distance(node1, node2):
-    return (node1.col-node2.col)**2 + (node1.row-node2.row)**2
+    return math.sqrt((node1.col-node2.col)**2 + (node1.row-node2.row)**2)
 
 # function for finding the direction from node 1 to node 2
 # Input: Node 1 and Node 2
@@ -32,6 +32,8 @@ class RRTBot:
         self.tree = Graph(start)
         self.nrow = nrow
         self.ncol = ncol
+        # self.collisions = 0
+        self.success = 0
     
     # one iteration of the RRT algorithm
     # Input: None
@@ -48,15 +50,18 @@ class RRTBot:
         #find closest node in tree to random node
         q_curr = self.find_closest_node(q_rand)
         dir = direction(q_curr, q_rand)
-        print("Direction = %f" % dir)
+        dis = distance(q_curr, q_rand)
         #attempt to grow tree in direction of q_rand
-        q_new = Node(row=round(q_curr.row + self.epsilon*math.sin(dir),2),col=round(q_curr.col +self.epsilon*math.cos(dir),2))
+        q_new = Node(row=round(q_curr.row + (self.epsilon%dis)*math.sin(dir),2),col=round(q_curr.col +(self.epsilon%dis)*math.cos(dir),2))
         possible_edge = (q_curr, q_new)
         if not self.will_collide(possible_edge, hwalls, vwalls) and not self.lies_on_edge(q_new):
             q_new.parent = q_curr
             self.tree.V.append(q_new)
             self.current_pos=q_new
             self.tree.E.append(possible_edge)
+            self.success+=1
+            # print(possible_edge[0])
+            # print(possible_edge[1])
             # self.plot()
 
     # method for finding closest node in tree to randomly generated node
@@ -87,23 +92,52 @@ class RRTBot:
         m = (edge[1].row-edge[0].row)/(edge[1].col-edge[0].col)
         #check collision with horizontal walls
         for wall in hwalls:
-            if wall.row in np.arange(min(edge[0].row, edge[1].row), max(edge[0].row, edge[1].row)+1, 0.01):
+            l = min(edge[0].row, edge[1].row)
+            u = max(edge[0].row, edge[1].row)
+            # if wall.row in np.arange(min(edge[0].row, edge[1].row), max(edge[0].row, edge[1].row)+1, 0.01):
+            if wall.row >= l and wall.row <= u:
+                print("Possible collision!")
+                # lower_node = min(edge[0].col, edge[1].col)
+                # upper_node = max(edge[0].col, edge[1].col)
+                # case1 = lower_node>wall.llim and upper_node<wall.ulim
+                # case2 = lower_node<wall.ulim and upper_node<wall.ulim
+                # case3 = lower_node>wall.llim and upper_node>wall.ulim
+                # if case1:
+                #     print("Collision!")
+                #     return True
                 y = wall.row
                 y1 = edge[0].row
                 x1 = edge[0].col
-                if (y-y1)/m+x1 in np.arange(wall.llim, wall.ulim+1, 0.01):
-                    print("Collision!")
+                point = round((y-y1)/m+x1,2)
+                # print(point)
+                # if round((y-y1)/m+x1,2) in np.arange(wall.llim, wall.ulim+1, 0.01):
+                if point >= wall.llim and point <= wall.ulim:
+                    print("H Collision!")
+                    # self.collisions += 1
                     return True
         #check collision with vertical walls
         for wall in vwalls:
-            if wall.col in np.arange(min(edge[0].col, edge[1].col), max(edge[0].col, edge[1].col)+1, 0.01):
+            l = min(edge[0].col, edge[1].col)
+            u = max(edge[0].col, edge[1].col)
+            # if wall.col in np.arange(min(edge[0].col, edge[1].col), max(edge[0].col, edge[1].col)+1, 0.01):
+            if wall.col >= l and wall.col <= u:
+                # lower_node = min(edge[0].row, edge[1].row)
+                # upper_node = max(edge[0].row, edge[1].row)
+                # case1 = lower_node>wall.llim and upper_node<wall.ulim
+                # if case1:
+                #     print("Collision!")
+                #     return True
                 x = wall.col
                 y1 = edge[0].row
                 x1 = edge[0].col
-                if m*(x-x1)+y1 in np.arange(wall.llim, wall.ulim+1, 0.01):
-                    print("Collision!")
+                point = round(m*(x-x1)+y1,2)
+                # print(point)
+                # if round(m*(x-x1)+y1,2) in np.arange(wall.llim, wall.ulim+1, 0.01):
+                if point >= wall.llim and point <= wall.ulim:
+                    print("V Collision!")
                     return True
         return False
+
     # method to determine if a node lies on a current edge of the tree
     # Input: node to add to tree
     # Output: Boolean (True: there is an edge that the node would coincide with, False: No edge)
@@ -122,15 +156,21 @@ class RRTBot:
                     print("On edge!")
                     return True
         return False
+    
+    
     def plot(self):
         # plot vertices
         for node in self.tree.V:
-            plt.plot(node.col, node.row, 'rx')
+            plt.plot(node.col, self.nrow-node.row, 'rx')
         for edge in self.tree.E:
             x_arr = [edge[0].col, edge[1].col]
-            y_arr = [edge[0].row, edge[1].row]
+            y_arr = [self.conv(edge[0].row), self.conv(edge[1].row)]
             plt.plot(x_arr, y_arr, 'bo', linestyle="--")
-        plt.show()
+        # plt.show()
+    
+    # method for converting row to numpy array reference frame
+    def conv(self, row):
+        return self.nrow - row
 def main():
     num_rows = 6 # Number of rows in the maze
     num_cols = 6 # Number of columns in the maze
@@ -149,21 +189,29 @@ def main():
     for i in range(num_rows):
 	    for j in range(num_cols):
 	    	field[i*2+1][j*2+1] = 1
-	# ---- Turn field into maze ----
     maze = maze_generation.random_kruskal_maze(field)
 	# ---- Increase maze size ----
     big_maze = maze_generation.maze_expansion(maze, num_inside)
     (hwalls, vwalls) = maze_generation.get_list_walls(big_maze)
+    print('HWalls: ')
+    for wall in hwalls:
+        print(wall)
+    print('VWalls: ')
+    for wall in vwalls:
+        print(wall)
 	# ---- Generate the starting fires
     maze_generation.generate_fires(big_maze, num_fires_smol, num_fires_med, num_fires_lrg)
 	# ---- Generate the entrance to the maze
     maze_generation.generate_entrances(big_maze, num_ent)
 	# ---- Plot maze ----
-    maze_generation.plot(big_maze)
+    # maze_generation.plot(big_maze)
     bot = RRTBot(epsilon= 3, start=Node(row=1, col=1), nrow=36, ncol=36)
-    for i in range(0,100):
-        print(i)
+    while bot.success<500:
+        # print(i)
         bot.rrt_move(hwalls=hwalls,vwalls=vwalls)
+    # print("Collisions: %d" % bot.collisions)
     bot.plot()
+    maze_generation.plot(big_maze)
+
 if __name__ == "__main__":
 	main()
